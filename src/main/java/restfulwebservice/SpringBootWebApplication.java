474f3898256a821011842f2aeb416856a98e5202
acer <acer@HP650-i3>
2018-05-05 12:40:06 +0200

    SpringBootWebApplication update class

--- src/main/java/restfulwebservice/SpringBootWebApplication.java
+++ src/main/java/restfulwebservice/SpringBootWebApplication.java
@@ -1,1 +1,13 @@
 
+package restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
